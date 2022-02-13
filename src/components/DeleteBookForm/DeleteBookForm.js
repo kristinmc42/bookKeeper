793508269f052daa2bcdac76a272393f023da3cc
kristinmc42 <mccollum.k.j@gmail.com>
2022-02-13 11:02:30 -0500

    deleted commented out code from DeleteBookForm

--- src/components/DeleteBookForm/DeleteBookForm.js
+++ src/components/DeleteBookForm/DeleteBookForm.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Link, useParams, useNavigate } from "react-router-dom";
-// import { useState, useEffect } from "react";
 import firebase from "../../firebase.js";
 import Button from "../Button/Button";
 import Card from "../Card/Card.js";
@@ -8,23 +7,11 @@ import "./DeleteBookForm.scss";
 
 
 function DeleteBookForm() {
-  // init state for showing this delete book form
-  // const [ showForm , setShowForm ] = useState(false);
-
    // init use params
   const { id } = useParams();
 
    // init history
   let navigate = useNavigate();
-
-  // change state based on classTitle (hide or show)
-  // useEffect(() => {
-  //   if (classTitle === "show"){
-  //     setShowForm(true)
-  //   }else {
-  //     setShowForm(false)
-  //   }
-  // }, [classTitle])
 
   // when user selects yes to delete title
   const handleDeleteTitle = () => {
@@ -34,18 +21,9 @@ function DeleteBookForm() {
     // delete title from database
     dbItem.remove();
 
-    
     // route back to Home
     navigate("/");    
   }
-
-  // when user selects no to delete title
-  // const handleCloseWindow = () => {
-  //   // hide the delete form
-  //   setShowForm(false);
-  //   //change state that will be passed back to parent so can click on delete button again
-  //   setVisible(false);
-  // }
 
   return(
     <>
